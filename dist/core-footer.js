b86d13d12838ca7c87b768f7f98c3cee8bf138ea
Emil Wibe <mail@emilwibe.dk>
2018-08-10 19:40:04 +0200

    front page

--- dist/core-footer.js
+++ dist/core-footer.js
@@ -1,3 +1,12 @@
 (function(){
-    console.log("hello");
+    //DOM ELEMENTS
+    var featuredPlaceholder = document.getElementById("featured-placeholder"),
+        featuredContainer = document.getElementById("featured-container")
+    ;
+
+    if(featuredPlaceholder){
+        featuredPlaceholder.addEventListener("load", function(e){
+            featuredContainer.style.backgroundImage = "url(" + featuredPlaceholder.getAttribute("src") + ")";
+        }, false);
+    }
 })();
