9d0f8c36b5357143fe312e6b28847a2762573af7
Emil Wibe <mail@emilwibe.dk>
2018-08-10 21:01:43 +0200

    front page

--- dist/core-footer.js
+++ dist/core-footer.js
@@ -2,13 +2,15 @@
     //DOM
     var featuredPlaceholder = document.getElementById("featured-placeholder"),
         featuredPlaceholderSrc,
-        featuredContainer = document.getElementById("featured-container")
+        featuredContainer = document.getElementById("featured-container"),
+        eventTitle = document.getElementById("event-title")
     ;
 
     function featuredImageLoad(){
         featuredPlaceholderSrc = featuredPlaceholder.getAttribute("src");
         featuredContainer.style.backgroundImage = 'url(' + featuredPlaceholderSrc + ')';
         featuredContainer.style.animationName = "fade-in";
+        eventTitle.style.animationName = "fade-in";
     }
 
     if(featuredPlaceholder && featuredContainer){
