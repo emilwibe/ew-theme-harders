3ee6cd5e93c63fccc296807482f62529677ee082
Emil Wibe <mail@emilwibe.dk>
2018-08-14 11:30:17 +0200

    horizontal scroll on front page

--- dist/core-footer.js
+++ dist/core-footer.js
@@ -78,6 +78,7 @@
         document.addEventListener("wheel", function(e){
             scrollMax = featuredSlider.scrollWidth - featuredSlider.clientWidth;
             if(scrollNum > 0 || scrollNum < scrollMax){
+                console.log("scrollNum " + scrollNum);
                 scrollNum += e.deltaX + e.deltaY;
                 featuredSlider.scrollLeft = scrollNum;
             }  
