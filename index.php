eb561d0ee5e7943319a017e22cc216079f42a459
Emil Wibe <mail@emilwibe.dk>
2018-08-15 13:12:22 +0200

    thumbnail on index

--- index.php
+++ index.php
@@ -6,4 +6,18 @@
     ]);
 ?>
 
+<?php if(have_posts()) : ?>
+
+    <main class="wrapper sm">
+    <?php while(have_posts()) : the_post(); ?>
+
+        <h1><?php the_title(); ?></h1>
+
+        <div><?php the_content(); ?></div>
+
+    <?php endwhile; ?>
+
+    </main><!--/.wrapper.sm-->
+<?php endif; ?>
+
 <?php get_footer(); ?>
