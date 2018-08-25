ebcd52b9247aa22bb1108249584a9ac8184a8773
Emil Wibe <mail@emilwibe.dk>
2018-08-25 19:32:12 +0200

    club25 functionality

--- template-parts/event-archive.php
+++ template-parts/event-archive.php
@@ -47,7 +47,8 @@
         <div class="featured wrapper sm2" id="featured-wrapper">
         <a class="featured event link" id="featured-link" href="<?php echo get_the_permalink(); ?>">
             <h2 class="featured event title" id="featured-title" style="color:<?php the_field('event_text_color'); ?>"><?php the_title(); ?></h2>
-            <time class="featured event date" id="featured-date"><?php $date = date_create(get_field('event_datetime')); echo date_format($date, "d/m"); ?></time><div class="club25" data-club="<?php the_field('event_klub_25'); ?>">Club<br><span>25</span></div>
+            <time class="featured event date" id="featured-date"><?php $date = date_create(get_field('event_datetime')); echo date_format($date, "d/m"); ?></time>
+            <div class="klub25" data-klub="<?php the_field('event_klub_25'); ?>">Klub25</div>
         </a>
         </div><!--/.wrapper.sm-->
 
@@ -94,10 +95,10 @@
             "eventThumbnail": "<?php echo get_the_post_thumbnail_url('', 'full'); ?>",
 <?php if(get_field('event_klub_25')) : ?>
             
-            "club25": true 
+            "klub25": true 
 <?php else : ?>
 
-            "club25": false
+            "klub25": false
 <?php endif; ?>
 
         }
