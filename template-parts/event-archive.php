def32b97cf2254ae0d559f8d45e835c7ea484449
Emil Wibe <mail@emilwibe.dk>
2018-08-25 19:11:13 +0200

    club25 functionality

--- template-parts/event-archive.php
+++ template-parts/event-archive.php
@@ -49,6 +49,7 @@
             <h2 class="featured event title" id="featured-title" style="color:<?php the_field('event_text_color'); ?>"><?php the_title(); ?></h2>
             <time class="featured event date" id="featured-date"><?php $date = date_create(get_field('event_datetime')); echo date_format($date, "d/m"); ?></time>
         </a>
+        <div class="club25" data-club="<?php the_field('event_klub_25'); ?>">Club<br><span>25</span></div>
         </div><!--/.wrapper.sm-->
 
     <?php endwhile; ?>
