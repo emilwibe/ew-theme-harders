716b9a2207dd2a32b2c131b8c49fc850091abc04
Emil Wibe <mail@emilwibe.dk>
2019-03-04 14:18:53 +0100

    s

--- screen.php
+++ screen.php
@@ -5,10 +5,28 @@
 <?php get_template_part('template-parts/event', 'archive'); ?>
 
 <script>
+    var
+        previous = null,
+        current = null
+    ;
+
     fetch('https://harders.nu/wp-json/wp/v2/event')
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(myJson){
+        previous = JSON.stringify(myJson);
+        console.log(previous);
+    })
+    ;
+/*
+    setInterval(function(){
+
+    }, 5000);
+    /*
     .then(function(response) {
         location.reload(true);
-    });
+    });*/
 </script>
 
 <?php get_footer(); ?>
