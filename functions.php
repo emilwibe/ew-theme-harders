518411f591d34a2abe6c968304e249d4c4c098bc
Emil Wibe <mail@emilwibe.dk>
2019-03-04 14:36:05 +0100

    s

--- functions.php
+++ functions.php
@@ -43,6 +43,8 @@
     //ADD EVENT CUSTOM FIELDS TO REST API
     add_action('rest_api_init', function(){
         register_rest_field('event', 'event_date',array(
-            'get_callback' => function(){return 'super';}
+            'get_callback' => function(){
+                return get_field('event_datetime');
+            }
         ));
     });
