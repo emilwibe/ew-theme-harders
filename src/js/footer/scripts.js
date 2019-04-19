91b31b52078d3e172e32829c1e1d66fbb9309360
Emil Wibe <mail@ew.dk>
2019-04-19 21:04:46 +0200

    s

--- src/js/footer/scripts.js
+++ src/js/footer/scripts.js
@@ -61,6 +61,8 @@
                 } else {
                     featuredEventCounter += 1;
                 }
+
+                console.log("Counter: " + featuredEventCounter);
             }, 1000);
             
         }
