bf9d2d4ea86c98b356167f593aa1085e13c67e9c
Emil Wibe <mail@ew.dk>
2020-05-08 18:14:02 +0200

    s

--- dist/js/scripts-footer.js
+++ dist/js/scripts-footer.js
@@ -79,11 +79,14 @@
             }
         }
 
-        featuredWrapper.style.animationName = "fade-in";
-        featuredWrapper.style.animationDelay = "0";
-        featuredTitle.style.animationName = "fade-in";
-        featuredDate.style.animationName = "fade-in";
-        featuredKlub.style.animationName = "fade-in";
+        if ( featuredWrapper ) {
+            featuredWrapper.style.animationName = "fade-in";
+            featuredWrapper.style.animationDelay = "0";
+            featuredTitle.style.animationName = "fade-in";
+            featuredDate.style.animationName = "fade-in";
+            featuredKlub.style.animationName = "fade-in";
+        }
+        
 
         setInterval(featuredSwitchContent, 10000);
     
