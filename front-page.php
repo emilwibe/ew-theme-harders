4f2015bd0ccde7814d23a5bb9440c2552129a8d2
Emil Wibe <mail@ew.dk>
2020-05-08 18:53:03 +0200

    s

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-<?php // get_template_part('template-parts/event', 'archive'); ?>
+<?php  get_template_part('template-parts/event', 'archive'); ?>
 
-<?php get_template_part('template-parts/index', 'live'); ?>
+<?php // get_template_part('template-parts/index', 'live'); ?>
 
 <?php get_footer(); ?>
