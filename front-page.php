ca9d0819ce392317a2432dbc8f132a19eed1dab1
Emil Wibe <mail@ew.dk>
2020-05-08 18:57:27 +0200

    s

--- front-page.php
+++ front-page.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 
-<?php  get_template_part('template-parts/event', 'archive'); ?>
-
-<?php // get_template_part('template-parts/index', 'live'); ?>
+<?php
+  if ( get_field( 'live-feed_embed-code' ) ) {
+    get_template_part('template-parts/index', 'live');
+  } else {
+    get_template_part('template-parts/event', 'archive');
+  }
+?>
 
 <?php get_footer(); ?>
