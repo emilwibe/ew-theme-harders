f27e475776f4ff41570250ebd3d396a6226090b7
Emil Wibe <mail@ew.dk>
2020-05-08 18:23:34 +0200

    s

--- template-parts/index-live.php
+++ template-parts/index-live.php
@@ -1,7 +1,7 @@
 <div class="wrapper xl">
 
 <div class="embed-feed fb-live">
-<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fspillestedetharders%2Fvideos%2F2602653939981556%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+  <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fspillestedetharders%2Fvideos%2F2602653939981556%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
 </div><!--/.embed-feed-->
 
 
