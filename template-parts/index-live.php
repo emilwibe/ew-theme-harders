715565ab54601eec92c6e1a6ed60ad1d37163852
Emil Wibe <mail@ew.dk>
2020-05-08 18:17:36 +0200

    s

--- template-parts/index-live.php
+++ template-parts/index-live.php
@@ -1,3 +1,9 @@
+<div class="wrapper xl">
+
+<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fspillestedetharders%2Fvideos%2F2602653939981556%2F&width=0" width="0" height="0" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+
+</div><!--/.wrapper-->
+
 <?php
   $months = array();
   $months[1] = "Januar";
