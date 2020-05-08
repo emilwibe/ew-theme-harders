182c110f87846d2d9cbff4ad94313ae0f2c80a64
Emil Wibe <mail@ew.dk>
2020-05-08 18:59:56 +0200

    s

--- front-page.php
+++ front-page.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 
 <?php
+
   if ( get_field( 'live-feed_embed-code', 'option' ) ) {
     get_template_part('template-parts/index', 'live');
   } else {
     get_template_part('template-parts/event', 'archive');
   }
+  
 ?>
-
+<!-- hello there <?php echo get_field( 'live-feed_embed-code', 'option' ); ?> -->
 <?php get_footer(); ?>
