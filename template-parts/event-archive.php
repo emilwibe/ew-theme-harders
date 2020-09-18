9b2d3766a865813c620de2998286ceb922baa40b
Emil Wibe <mail@ew.dk>
2020-09-18 12:57:22 +0200

    [refactor]: preparing for more event preferences

--- template-parts/event-archive.php
+++ template-parts/event-archive.php
@@ -1,123 +1,138 @@
 <?php
-  $months = array();
-  $months[1] = "Januar";
-  $months[2] = "Februar";
-  $months[3] = "Marts";
-  $months[4] = "April";
-  $months[5] = "Maj";
-  $months[6] = "Juni";
-  $months[7] = "Juli";
-  $months[8] = "August";
-  $months[9] = "September";
-  $months[10] = "Oktober";
-  $months[11] = "November";
-  $months[12] = "December";
+$months = array();
+$months[1] = "Januar";
+$months[2] = "Februar";
+$months[3] = "Marts";
+$months[4] = "April";
+$months[5] = "Maj";
+$months[6] = "Juni";
+$months[7] = "Juli";
+$months[8] = "August";
+$months[9] = "September";
+$months[10] = "Oktober";
+$months[11] = "November";
+$months[12] = "December";
 
-  $date_now = date('Y-m-d H:i:s');
-  $time_now = strtotime($date_now);
+$date_now = date('Y-m-d H:i:s');
+$time_now = strtotime($date_now);
 
-  $time_next_year = strtotime('+365 day', $time_now);
-  $date_next_year = date('Y-m-d H:i:s', $time_next_year);
+$time_next_year = strtotime('+365 day', $time_now);
+$date_next_year = date('Y-m-d H:i:s', $time_next_year);
 
-  $args = array(
-      'post_type' => 'event',
-      'posts_per_page' => 1,
-      'meta_query' => array(
-          array(
-            'key' => 'event_datetime',
-            'compare' => 'BETWEEN',
-            'value' => array($date_now, $date_next_year),
-            'type' => 'DATETIME'
-          )
-        ),
-        'order' => 'ASC',
-        'orderby' => 'meta_value',
-        'meta_key' => 'event_datetime',
-        'meta_type' => 'DATETIME'
-  );
+$args = array(
+	'post_type' => 'event',
+	'posts_per_page' => 1,
+	'meta_query' => array(
+		array(
+			'key' => 'event_datetime',
+			'compare' => 'BETWEEN',
+			'value' => array($date_now, $date_next_year),
+			'type' => 'DATETIME'
+		)
+	),
+	'order' => 'ASC',
+	'orderby' => 'meta_value',
+	'meta_key' => 'event_datetime',
+	'meta_type' => 'DATETIME'
+);
 
-  $the_query = new WP_Query($args);
-  ?>
-  <?php if($the_query->have_posts()) : ?>
-    <?php while($the_query->have_posts()) : $the_query->the_post(); ?>
-    <?php
-      $eventPreferences = get_field( 'event_klub_25' );
-    ?>
-
-        <img src="" alt="" class="featured placeholder" id="featured-placeholder">
-        <div class="featured image container" id="featured-container" style="background-image:url('<?php echo get_the_post_thumbnail_url(null, 'full'); ?>'); ?>"></div><!--/.featured.image-->
-
-        <div class="featured wrapper sm2" id="featured-wrapper">
-        <a class="featured event link" id="featured-link" href="<?php echo get_the_permalink(); ?>">
-            <h2 class="featured event title" id="featured-title" style="color:<?php the_field('event_text_color'); ?>"><?php the_title(); ?></h2>
-            <time class="featured event date" id="featured-date"><?php $date = date_create(get_field('event_datetime')); echo date_format($date, "d/m"); ?></time>
-
-        <?php if( $eventPreferences[0] === 'club25' ) : ?>
-
-            <div class="klub25" id="featured-klub" data-klub="<?php the_field('event_klub_25'); ?>">Klub25</div>
-        <?php endif; ?>
-        
-        </a>
-      </div><!--/.wrapper.sm-->
-
-    <?php endwhile; ?>
-    <?php wp_reset_postdata(); ?>
-  <?php endif; ?>
-
-    <div class="featured event slider" id="featured-slider">
-
-    </div><!--/.featured.event.slider-->
-
-  <?php
-    $args = array(
-        'post_type' => 'event',
-        'posts_per_page' => 200,
-        'meta_query' => array(
-            array(
-              'key' => 'event_datetime',
-              'compare' => 'BETWEEN',
-              'value' => array($date_now, $date_next_year),
-              'type' => 'DATETIME'
-            )
-          ),
-        'order' => 'ASC',
-        'orderby' => 'meta_value',
-        'meta_key' => 'event_datetime',
-        'meta_type' => 'DATETIME'
-    );
-    $the_query = new WP_Query($args);
+$the_query = new WP_Query($args);
 ?>
- 
- <?php //ARRAY OF EVENT OBJECT ?>
- <?php if($the_query->have_posts()) : ?>
-    
-    <script>
-        var featuredEventArray = [],
-            eventSingle
-        ;
-    <?php while($the_query->have_posts()) : $the_query->the_post(); ?>
-        eventSingle = {
-            "eventTitle": "<?php the_title(); ?>",
-            "eventTime": "<?php $date = date_create(get_field('event_datetime')); echo date_format($date, "d/m"); ?>",
-            "eventLink": "<?php echo get_the_permalink(); ?>",
-            "eventThumbnail": "<?php echo get_the_post_thumbnail_url('', 'full'); ?>",
+<?php if ($the_query->have_posts()) : ?>
+	<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+		<?php
+		$eventPreferences = get_field('event_klub_25');
+		?>
 
-<?php
-  $eventPreferences = get_field( 'event_klub_25' );
-?>
+		<img src="" alt="" class="featured placeholder" id="featured-placeholder">
+		<div class="featured image container" id="featured-container" style="background-image:url('<?php echo get_the_post_thumbnail_url(null, 'full'); ?>'); ?>"></div>
+		<!--/.featured.image-->
 
-<?php if( $eventPreferences[0] === 'club25' ) : ?>
-            
-            "klub25": true 
-<?php else : ?>
+		<div class="featured wrapper sm2" id="featured-wrapper">
+			<a class="featured event link" id="featured-link" href="<?php echo get_the_permalink(); ?>">
+				<h2 class="featured event title" id="featured-title" style="color:<?php the_field('event_text_color'); ?>"><?php the_title(); ?></h2>
+				<time class="featured event date" id="featured-date"><?php $date = date_create(get_field('event_datetime'));
+																		echo date_format($date, "d/m"); ?></time>
 
-            "klub25": false
+				<?php if ($eventPreferences[0] === 'club25') : ?>
+
+					<div class="klub25" id="featured-klub" data-klub="<?php the_field('event_klub_25'); ?>">Klub25</div>
+				<?php endif; ?>
+				<?php if ($eventPreferences[0] === 'fynlive' || $eventPreferences[0] === 'fynlive') : ?>
+
+					<div class="klub25 fynlive" id="featured-klub" data-klub="fynlive">FynLive</div>
+				<?php endif; ?>
+
+			</a>
+		</div>
+		<!--/.wrapper.sm-->
+
+	<?php endwhile; ?>
+	<?php wp_reset_postdata(); ?>
 <?php endif; ?>
 
-        }
-        featuredEventArray.push(eventSingle);
-    <?php endwhile; ?>
-    <?php wp_reset_postdata(); ?>
+<div class="featured event slider" id="featured-slider">
 
-    </script>
+</div>
+<!--/.featured.event.slider-->
+
+<?php
+$args = array(
+	'post_type' => 'event',
+	'posts_per_page' => 200,
+	'meta_query' => array(
+		array(
+			'key' => 'event_datetime',
+			'compare' => 'BETWEEN',
+			'value' => array($date_now, $date_next_year),
+			'type' => 'DATETIME'
+		)
+	),
+	'order' => 'ASC',
+	'orderby' => 'meta_value',
+	'meta_key' => 'event_datetime',
+	'meta_type' => 'DATETIME'
+);
+$the_query = new WP_Query($args);
+?>
+
+<?php //ARRAY OF EVENT OBJECT 
+?>
+<?php if ($the_query->have_posts()) : ?>
+
+	<script>
+		var featuredEventArray = [],
+			eventSingle;
+		<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+			eventSingle = {
+				"eventTitle": "<?php the_title(); ?>",
+				"eventTime": "<?php $date = date_create(get_field('event_datetime'));
+								echo date_format($date, "d/m"); ?>",
+				"eventLink": "<?php echo get_the_permalink(); ?>",
+				"eventThumbnail": "<?php echo get_the_post_thumbnail_url('', 'full'); ?>",
+
+				<?php
+				$eventPreferences = get_field('event_klub_25');
+				?>
+
+				<?php if ($eventPreferences[0] === 'club25') : ?>
+
+					"klub25": true,
+				<?php else : ?>
+
+					"klub25": false,
+				<?php endif; ?>
+				<?php if ($eventPreferences[0] === 'fynlive' || $eventPreferences[1] === 'fynlive') : ?>
+
+					"fynlive": true
+				<?php else : ?>
+
+					"fynlive": false
+				<?php endif; ?>
+
+			}
+			featuredEventArray.push(eventSingle);
+		<?php endwhile; ?>
+		<?php wp_reset_postdata(); ?>
+	</script>
 <?php endif; ?>
