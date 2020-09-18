8788877978350838fa8bb2261ea6a4421998b2da
Emil Wibe <mail@ew.dk>
2020-09-18 12:24:18 +0200

    [hotfix]: front page video

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 
 <?php
 
-  if ( get_field( 'live-feed_embed-code', 'option' ) ) {
+  if ( get_field( 'live-feed_embed-code', 'option' ) && get_field( 'live-feed_embed' ) ) {
     get_template_part('template-parts/index', 'live');
   } else {
     get_template_part('template-parts/event', 'archive');
